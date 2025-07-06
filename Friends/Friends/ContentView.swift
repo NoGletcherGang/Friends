66f5999c334745fd38c1cf4f224e6821f42d2e14
setuper <setuper@MacBookPro.home>
2025-07-06 23:55:00 +0300

    ci test

--- Friends/Friends/ContentView.swift
+++ Friends/Friends/ContentView.swift
@@ -2,9 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("check organization pt 2")
-        
-        Text("check organization pt 3")
+        Text("ci test")
     }
 }
 
